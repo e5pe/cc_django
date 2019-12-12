88ca77b9ec4ea741db3b362132e0556a1f0aac33
Esperanza Cutillas Rastoll <especr101@gmail.com>
2019-12-12 19:47:57 +0100

    Working with questions' detail view.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,20 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 
 def index(request):
-    last_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in last_question_list])
-    return HttpResponse(output)
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'lastest_question_list': lastest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question':question})
 
 
 def results(request, question_id):
